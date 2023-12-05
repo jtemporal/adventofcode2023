a2e3addd94ef839795d062d59369289443bd0540
Jessica Temporal <jessicatemporal@gmail.com>
2023-12-05 10:51:21 -0500

    Refactors day 2 part 1 to use helper module

--- day_2/part1.py
+++ day_2/part1.py
@@ -1,9 +1,5 @@
+from aoc.helper import read_input
 
-def read_input(doc_path):
-    with open(doc_path) as f:
-        values = f.readlines()
-    values = [value.strip('\n') for value in values]
-    return values
 
 games = read_input("day_2/input.txt")
 
